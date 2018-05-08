f91e9fecd950920713ca944260a3b330ff5a04b1
LENOVO-MIIX-510\enri1 <enricomenegatti13@gmail.com>
2018-05-08 15:41:52 +0200

    aggiunta stampa ingressi. rimossi provvisoriamente interrupt.

--- Sponda/Sponda.ino
+++ Sponda/Sponda.ino
@@ -15,7 +15,9 @@ int FC_UP_DX = 12;//NA GND
 int FC_DW_DX = 13;//NA GND
 
 bool lampeggia, led_state = 0, alza_sponda = 0, abbassa_sponda = 0, commuta_AB;
+bool su, giu, fc_up_sx, fc_dw_sx, fc_up_dx, fc_dw_dx;
 
+int This_ing, Last_ing;
 
 long this_time, last_time;
 
@@ -34,18 +36,41 @@ void setup()
   pinMode(GIU, INPUT);
 //-------------------Finecorsa------------------------
   pinMode(FC_UP_SX, INPUT_PULLUP);
-  pinMode(FC_UP_SX, INPUT_PULLUP);
-  pinMode(FC_UP_SX, INPUT_PULLUP);
-  pinMode(FC_UP_SX, INPUT_PULLUP);
+  pinMode(FC_DW_SX, INPUT_PULLUP);
+  pinMode(FC_UP_DX, INPUT_PULLUP);
+  pinMode(FC_DW_DX, INPUT_PULLUP);
   
-  attachInterrupt(digitalPinToInterrupt(EMERGENZA), Emergenza, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(GIU), Abbassa, LOW);
+  //attachInterrupt(digitalPinToInterrupt(EMERGENZA), Emergenza, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(GIU), Abbassa, LOW);
 
   this_time = millis();
+
+  digitalWrite(R_LED, LOW);
+  digitalWrite(R_A, LOW);
+  digitalWrite(R_B, LOW);
+  digitalWrite(R_C, LOW);
+  digitalWrite(R_D, LOW);
 }
 
 void loop() 
 {
+  su = digitalRead(SU);
+  giu = digitalRead(GIU);
+  fc_up_sx = digitalRead(FC_UP_SX);
+  fc_dw_sx = digitalRead(FC_DW_SX);
+  fc_up_dx = digitalRead(FC_UP_DX);
+  fc_dw_dx = digitalRead(FC_DW_DX);
+  
+  This_ing = (su + (10 * giu) + (100 * fc_up_sx) + (1000 * fc_dw_sx) + (10000 * fc_up_dx) + (100000 * fc_dw_dx)); 
+
+  if (This_ing != Last_ing)
+  {
+    Print();
+    Last_ing = This_ing;
+  }
+
+  //digitalWrite(R_LED, digitalRead(SU));
+  
 //--------------------------lampeggia led se emergenza-------------------------
   if (lampeggia == 1)
   {
@@ -71,7 +96,7 @@ void loop()
     
     delay (50);
 
-    while(GIU == 0 && FC_DW_SX == 0 && FC_DW_DX == 0)//finchè premuto e no finecorsa
+    while(giu == 0 && fc_dw_sx == 0 && fc_dw_dx == 0)//finchè premuto e no finecorsa
     {
       digitalWrite(R_C, HIGH);
       digitalWrite(R_LED, HIGH);
@@ -97,7 +122,7 @@ void loop()
     
     delay (50);
 
-    while(SU == 0 && FC_UP_SX == 0 && FC_UP_DX == 0)//finchè premuto e no finecorsa
+    while(su == 0 && fc_up_sx == 0 && fc_up_dx == 0)//finchè premuto e no finecorsa
     {
       digitalWrite(R_C, HIGH);
       digitalWrite(R_LED, HIGH);
@@ -109,10 +134,12 @@ void loop()
   }
 }
 
+//---------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
 //--------------------------routine di interrupt emergenza-------------------------
 void Emergenza()
 {
-  if (EMERGENZA == 1)
+  if (digitalRead(EMERGENZA) == 1)
   {
     digitalWrite(R_C, LOW);
     digitalWrite(R_D, LOW);
@@ -131,7 +158,11 @@ void Emergenza()
 //--------------------------routine di interrupt abbassa sponda-----------------------
 void Abbassa()
 {
-  if (GIU == 0 && FC_DW_SX == 0 && FC_DW_DX == 0)//se si può abbassare
+  giu = digitalRead(GIU);
+  fc_dw_sx = digitalRead(FC_DW_SX);
+  fc_dw_dx = digitalRead(FC_DW_DX);
+  
+  if (giu == 0 && fc_dw_sx == 0 && fc_dw_dx == 0)//se si può abbassare
   {
     digitalWrite(R_C, LOW);
 
@@ -147,7 +178,11 @@ void Abbassa()
 //--------------------------routine alza sponda-----------------------
 void Alza()
 {
-  if (SU == 0 && FC_UP_SX == 0 && FC_UP_DX == 0)//se si può alzare
+  su = digitalRead(SU);
+  fc_up_sx = digitalRead(FC_UP_SX);
+  fc_up_dx = digitalRead(FC_UP_DX);
+  
+  if (su == 0 && fc_up_sx == 0 && fc_up_dx == 0)//se si può alzare
   {
     digitalWrite(R_C, LOW);
 
@@ -157,5 +192,23 @@ void Alza()
     }
     alza_sponda = 1;
   }
+}
+
+void Print()
+{
+  Serial.println("---------------------------------------------------------");
+  Serial.print("su");
+  Serial.println(su);
+  Serial.print("giu");
+  Serial.println(giu);
+  Serial.print("fc_up_sx");
+  Serial.println(fc_up_sx);
+  Serial.print("fc_dw_sx");
+  Serial.println(fc_dw_sx);
+  Serial.print("fc_up_dx");
+  Serial.println(fc_up_dx);
+  Serial.print("fc_dw_dx");
+  Serial.println(fc_dw_dx);
+  Serial.println("---------------------------------------------------------");
 }
 
